54f41b4104503dae09b8d2d0a6e6553702ec90bf
Itzmepapa123 <absorbinghunger@gmail.com>
2024-07-18 14:05:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,15 +2,17 @@ import os
 
 
 class Config(object):
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    TELEGRAM_API = os.environ.get("TELEGRAM_API")
-    OWNER = os.environ.get("OWNER")
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
-    PASSWORD = os.environ.get("PASSWORD")
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-    LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
-    GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
     IS_PREMIUM = False
     MODES = ["video-video", "video-audio", "video-subtitle", "extract-streams"]
+    API_HASH = "fa4c3f582286d969ab1d08449e9533e8"
+    BOT_TOKEN = "7277558508:AAGq41OAe9SchpFgj115FUHZMHY7oV_1qoE"
+    TELEGRAM_API = "21310924"
+    OWNER = "7030439873"
+    OWNER_USERNAME = "Mr_Bankaiiii"
+    PASSWORD = "Bankai Op"
+    DATABASE_URL = "mongodb+srv://itzmeproman:itzmeproman@cluster0.4v4f5ao.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    LOGCHANNEL = "-1002174195692"    # Premium account session string to upload upto 4GB (requires "LOGCHANNEL")
+    
+    UPSTREAM_REPO = "https://github.com/Itzmepapa123/Merge"
+    UPSTREAM_BRANCH = "master"
